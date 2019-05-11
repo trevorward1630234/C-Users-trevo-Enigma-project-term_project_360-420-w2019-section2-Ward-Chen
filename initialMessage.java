d1296cdd064856246add5ff9c46ca9d5021ed227
Jacky2309 <50172703+Jacky2309@users.noreply.github.com>
2019-05-11 19:56:04 -0400

    Add files via upload

---
+++ initialMessage.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; //to include 
+
+public class initialMessage
+{
+	public static void main (String[] args)
+	{
+		String[] inMessArr = new String[]; //inMessArr = initial Message array
+		in.length = 
+		
+		while(true){
+			
+			Scanner keyboard = new Scanner(System.in);
+			system.out.prntln("Please enter the message letter by letter. If the message is over, type '0'.");
+			inMessArr = keyboard.nextLine();
+		
+			if(imput.equals("0")){
+				break;
+			}
+		}
+	}
+}
