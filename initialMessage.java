c3d2b61cceaaa497ee578903ba34207b4b29c19a
Jacky2309 <50172703+Jacky2309@users.noreply.github.com>
2019-05-12 22:12:48 -0400

    Update initialMessage.java

--- initialMessage.java
+++ initialMessage.java
@@ -4,18 +4,25 @@ public class initialMessage
 {
 	public static void main (String[] args)
 	{
-		String[] inMessArr = new String[]; //inMessArr = initial Message array
-		inMessArr.length = 
+		int i = 1000000; //array size
+		String[] TempMessArr = new String[i]; //TempMessArr = temporary initial Message array
+		int count = 0;
 		
 		while(true){
 			
-			Scanner keyboard = new Scanner(System.in);
-			system.out.prntln("Please enter the message letter by letter. If the message is over, type '0'.");
-			inMessArr = keyboard.nextLine();
-		
-			if(imput.equals("0")){
+			Scanner Keyboard = new Scanner(System.in);
+			System.out.println("Please enter the message letter by letter. If the message is over, type '0'.");
+			TempMessArr[i] = Keyboard.nextLine();
+			count++;
+			
+			if(Keyboard.equals("0")){
 				break;
-			}
-		}
+		}}
+
+		String[] inMessArr = new String[count];
+		inMessArr = TempMessArr;
+		
+
+		System.out.print(inMessArr);
 	}
 }
